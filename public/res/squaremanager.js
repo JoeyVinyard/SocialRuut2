0b99fb01981a23f399f90f1d82e3d4781c6ed3e8
JoeyVinyard <jviny98@yahoo.com>
2016-08-03 14:42:56 -0400

    Added js files and express linking

---
+++ public/res/squaremanager.js
@@ -0,0 +1,9 @@
+var square = {
+	id		: 0,
+	gridXPos	: 0,
+	gridYPos	: 0,
+	drawXPos	: 0,
+	drawYPos	: 0,
+	color		: "#000000"
+}
+var clients = [];
--- screendraw.js
+++
@@ -1,8 +0,0 @@
-//prepCanv();
-console.log("hello world");
-function prepCanv(){
-	var canvas = document.getElementById("canv");
-	canvas.width = document.body.clientWidth;
-	canvas.height = document.body.clientHeight;
-	var context = canvas.getContext("2d");
-}
