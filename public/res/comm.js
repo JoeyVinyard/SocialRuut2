71a581342f12eaf40d229008081059c2cf2b4144
JoeyVinyard <jviny98@yahoo.com>
2016-08-30 14:23:54 -0400

    Began local drawing

--- public/res/comm.js
+++ public/res/comm.js
@@ -1,11 +1,9 @@
 var server = io();
-console.log(server);
 server.on('initUser', function(clients){
 	for(var i = 1; i < clients.length; i++){
 		createNewSquare(clients[i-1]);
 	}
 	localSquareID = clients.length;
-
 });
 server.on('addNewUser', function(clientInfo){
 	createNewSquare(clientInfo);
--- public/res/screendraw.js
+++ public/res/screendraw.js
@@ -1,10 +1,12 @@
 var canvas;
 var context;
 
-prepCanv();
 function prepCanv(){
 	canvas = document.getElementById("canv");
 	$("#canv").attr("width",$(window).width());
 	$("#canv").attr("height",$(window).height());
 	context = canvas.getContext("2d");
+}
+function drawLocSquare(){
+	context.fillStyle = squares[localSquareID -1];
 }
